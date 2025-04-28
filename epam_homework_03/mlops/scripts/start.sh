05b802721866a2e117463d806617b7fb2cb73cec
Edgar Vardanyan <e.vardan4an@gmail.com>
2025-04-28 19:00:37 +0400

    add mage ai

---
+++ epam_homework_03/mlops/scripts/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# PROJECT_NAME=mlops \
+#   MAGE_CODE_PATH=/home/src \
+#   SMTP_EMAIL=$SMTP_EMAIL \
+#   SMTP_PASSWORD=$SMTP_PASSWORD \
+
+
+export PROJECT_NAME=mlops
+export MAGE_CODE_PATH=/home/src
+export  SMTP_EMAIL=
+export  SMTP_PASSWORD=
+export PYTHONPATH="/home/src/mlops:${PYTHONPATH}"
+docker compose up
